e50807a1bb6ee17817b2d7665dce6e322d08b0b0
Matthew Edge-Williams <mat.edge@gmail.com>
2017-02-13 13:39:12 +1100

    first location see

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+l1 = Location.create :name => 'Sydney', :latitude => '-33.865143', :longitude: => '151.209900'
